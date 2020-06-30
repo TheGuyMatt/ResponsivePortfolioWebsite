6a5eae0a1ddece6eef32426bf3ea3db640663eef
TheGuyMatt <matthew.hieb@protonmail.com>
2020-06-30 14:05:46 -0700

    Added cursor blinking animation for hello world text

--- root/script/script.js
+++ root/script/script.js
@@ -1,1 +1,15 @@
-//script things
+var helloworldtext = document.getElementById("helloworldtext");
+
+function toggleTextCursor()
+{
+  if (helloworldtext.style.color == "white")
+  {
+    helloworldtext.style.color = "black";
+  }
+  else
+  {
+    helloworldtext.style.color = "white";
+  }
+}
+
+setInterval(toggleTextCursor, 500);
